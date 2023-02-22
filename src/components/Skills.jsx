2d5b01c13973d2cc824136e631c2490b566031e8
Aditya Nair <adinair.in@gmail.com>
2023-02-21 18:31:23 -0600

    Reduced the number of skills to avoid overlapping

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,10 +16,10 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-              <p className='py-4'>// These are the technologies I have experience with</p>
+              <p className='py-3'>// These are some of the technologies I have experience with</p>
           </div>
 
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <div className='w-full grid grid-cols-2 sm:grid-cols-6 gap-4 text-center py-8'>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                   <p className='my-4'>HTML</p>
@@ -36,10 +36,10 @@ const Skills = () => {
                   <img className='w-20 mx-auto' src={Node} alt="NodeJs icon" />
                   <p className='my-4'>NodeJs</p>
               </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={MySQL} alt="MySQL icon" />
                   <p className='my-4'>MySQL</p>
-              </div>
+              </div> */}
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Mongo} alt="Mongo DB icon" />
                   <p className='my-4'>Mongo DB</p>
@@ -48,10 +48,10 @@ const Skills = () => {
                   <img className='w-20 mx-auto' src={reactImg} alt="React icon" />
                   <p className='my-4'>React</p>
               </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Github} alt="Github icon" />
                   <p className='my-4'>Github</p>
-              </div>
+              </div> */}
           </div>
       </div>
     </div>
