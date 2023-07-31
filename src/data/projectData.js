e1d602478441b57a496dbe73128658279f8580c1
Aditya Nair <adinair.in@gmail.com>
2023-07-31 15:24:42 -0500

    Updated projects to add cowtown fence website

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,5 +1,5 @@
 import Crypto from "../assets/Crypto.jpeg";
-import TTG from "../assets/TTG.png";
+import Cowtown from "../assets/Cowtown.png"
 import NoteTaker from "../assets/NoteTaker.png";
 import Weather from "../assets/Weather.png";
 import JATE from "../assets/JATE1.png";
@@ -7,20 +7,21 @@ import STG from "../assets/STG.png";
 import Mojave from "../assets/MojaveLogo2.png";
 
 export const projectData = [
+  
   {
     id: 1,
+    name: "Cowtown Fence TX",
+    image: Cowtown,
+    webpage: "https://cowtownfencetx.com",
+    github: "https://github.com/adinair007/cowtown-fence",
+  },
+  
+  {
+    id: 2,
     name: "MOJAVE",
     image: Mojave,
     webpage: "https://mojave-store.herokuapp.com/",
     github: "https://github.com/adinair007/Mojave",
-  },
-
-  {
-    id: 2,
-    name: "Just Another Text Editor",
-    image: JATE,
-    webpage: "https://just-another-text-editor007.herokuapp.com",
-    github: "https://github.com/adinair007/Just_Another_Text_Editor",
   },
 
   {
@@ -33,18 +34,18 @@ export const projectData = [
 
   {
     id: 4,
-    name: "The Spectacular Tech Blog",
-    image: STG,
-    webpage: "https://the-spectacular-tech-blog.herokuapp.com/",
-    github: "https://github.com/adinair007/The_Spectacular_Tech_Blog",
-  },
-
-  {
-    id: 5,
     name: "Weather Dashboard",
     image: Weather,
     webpage: "https://adinair007.github.io/Weather_Dashboard/",
     github: "https://github.com/adinair007/Weather_Dashboard",
+  },
+
+  {
+    id: 5,
+    name: "The Spectacular Tech Blog",
+    image: STG,
+    webpage: "https://the-spectacular-tech-blog.herokuapp.com/",
+    github: "https://github.com/adinair007/The_Spectacular_Tech_Blog",
   },
 
   {
